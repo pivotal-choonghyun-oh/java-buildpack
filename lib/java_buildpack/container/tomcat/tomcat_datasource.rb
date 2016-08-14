69d0a04a46021f6c99441100cb9d51122a69376b
Choonghyun Oh <coh@Choonghyun-Ohs-MacBook-Pro.local>
2016-08-14 15:35:45 +0900

    aa

--- lib/java_buildpack/container/tomcat/tomcat_datasource.rb
+++ lib/java_buildpack/container/tomcat/tomcat_datasource.rb
@@ -95,7 +95,7 @@ module JavaBuildpack
       def change_http_connector_encoding
         puts '        Add URIEncoding to conf/server.xml...'
         server_xml_doc = read_xml server_xml
-        connector_node = REXML::XPath.match(document, '/Server/Service/Connector').first
+        connector_node = REXML::XPath.match(server_xml_doc, '/Server/Service/Connector').first
         
         connector_node.add_attribute 'URIEncoding', 'UTF-8'
         
